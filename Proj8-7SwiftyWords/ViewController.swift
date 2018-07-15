660e240dec4f511f4aa166f25ffb5c895a4f7ff0
SKE <appybuildmore@gmail.com>
2018-07-15 05:27:01 -0500

    Initial Commit

---
+++ Proj8-7SwiftyWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proj8-7SwiftyWords
+//
+//  Created by Sain-R Edwards Jr. on 7/15/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
